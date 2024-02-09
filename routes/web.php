c5f0d894b03afa282c1266f7095a97210992c2ac
A.Mahidwei <xile.meko@gmail.com>
2024-02-09 21:19:22 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('/')
+    ->middleware('auth')
+    ->group(function () {
+        Route::resource('users', UserController::class);
+        Route::get('profile', [
+            \App\Http\Controllers\ProfileController::class,
+            'show',
+        ])->name('profile.show');
+        Route::put('profile', [
+            \App\Http\Controllers\ProfileController::class,
+            'update',
+        ])->name('profile.update');
+    });
