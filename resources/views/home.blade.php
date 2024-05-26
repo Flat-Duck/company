b6deefa5d79ce9b5c5151dfcac054412b5bcd2cc
A.Mahidwei <xile.meko@gmail.com>
2024-05-26 16:13:55 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,69 +7,69 @@
         <x-state color="bg-indigo-lt" icon="inbox" title="رسائل الوارد" subtitle="{{ \App\Models\Inbox::count() }}" />
         <x-state color="bg-blue-lt" icon="clipboard-typography" title="المعاملات الاخرى " subtitle="{{ \App\Models\Memo::count() }}" />
     </div>
+    
     <div class="row justify-content-center mt-4">
         <div class="col-md-12">
-            <div class="card card-sm">
+            <div class="card">
                 <div class="card-status-top bg-yellow"></div>
-                <div class="card-body">
+                <div class="card-body border-bottom py-3">
                     <div class="d-flex">
                         <div class="row g-2">
                             <div class="col">
                                 <h3 class="card-title">سجل حركة المستخدمين</h3>
                             </div>
                         </div>
+                        <div class="col-auto ms-auto d-print-none"></div>
                     </div>
-                    <div class="divide-y-2 mt-4">
-                        <div class="table-responsive">
-                            <table class="table" id="dataTable">
-                                <thead>
-                                    <tr>
-                                        <th class="text-left">
-                                            #
-                                        </th>
-                                        <th class="text-left">
-                                            المعاملة
-                                        </th>
-                                        <th class="text-left">
-                                            نوع العملية
-                                        </th>
-                                        <th class="text-left">
-                                            القائم بالعملية
-                                        </th>
-                                        <th class="text-left">
-                                            وصف العملية
-                                        </th>
-                                        <th class="text-center">
-                                            عرض
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse(\App\Models\Activity::latest()->get()  as $k=> $activity)
-                                        <tr>
-                                            <td>{{ $k+1 }}</td>
-                                            <td>{{ $activity->name ?? '-' }}</td>
-                                            <td>{{ $activity->type ?? '-' }}</td>
-                                            <td>{{ $activity->user->name ?? '-' }}</td>
-                                            <td>{{ $activity->description ?? '-' }}</td>
-                                            <td class="text-center" style="width: 134px;">
-                                                <div role="group" aria-label="Row Actions" class="btn-group">
-                                                    <a href="{{$activity->link}}" class="btn btn-icon btn-outline-info ms-1" >
-                                                        <i class="ti ti-eye"></i>
-                                                    </a>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @empty
-                                        <tr>
-                                            <td colspan="6">@lang('crud.common.no_items_found')</td>
-                                        </tr>
-                                        @endforelse
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table card-table table-vcenter text-nowrap datatable">
+                        <thead>
+                            <tr>
+                                <th class="text-left">
+                                    #
+                                </th>
+                                <th class="text-left">
+                                    المعاملة
+                                </th>
+                                <th class="text-left">
+                                    نوع العملية
+                                </th>
+                                <th class="text-left">
+                                    القائم بالعملية
+                                </th>
+                                <th class="text-left">
+                                    وصف العملية
+                                </th>
+                                <th class="text-center">@lang('crud.common.actions')</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($activities as $k=> $activity)
+                                <tr>
+                                    <td>{{ $k+1 }}</td>
+                                    <td>{{ $activity->name ?? '-' }}</td>
+                                    <td>{{ $activity->type ?? '-' }}</td>
+                                    <td>{{ $activity->user->name ?? '-' }}</td>
+                                    <td>{{ $activity->description ?? '-' }}</td>
+                                    <td class="text-center" style="width: 134px;">
+                                        <div role="group" aria-label="Row Actions" class="btn-group">
+                                            <a href="{{$activity->link}}" class="btn btn-icon btn-outline-info ms-1" >
+                                                <i class="ti ti-eye"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6">@lang('crud.common.no_items_found')</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer d-flex align-items-left">
+                    {!! $activities->render() !!}
                 </div>
             </div>
         </div>
